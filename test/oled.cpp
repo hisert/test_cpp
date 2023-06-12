3ac808dc2b80093a9dea3364cd4f34caf73fe936
hisert <100667448+hisert@users.noreply.github.com>
2023-06-12 13:11:49 +0300

    Update oled.cpp

--- test/oled.cpp
+++ test/oled.cpp
@@ -651,7 +651,7 @@ static void ssd1306_command(unsigned char command)
     i2c_write( control) ;
     i2c_write( command) ;
     i2c_stop() ;
-    usleep(100);
+    usleep(1000);
 }
 
 static void ssd1306_data(unsigned char value)
@@ -662,7 +662,7 @@ static void ssd1306_data(unsigned char value)
     i2c_write( control) ;
     i2c_write( value) ;
     i2c_stop() ;
-    usleep(100);
+    usleep(1000);
 } 
 };
 
