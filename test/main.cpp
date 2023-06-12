20663fdaf566ca862b1ebcba64f866be757c9426
hisert <100667448+hisert@users.noreply.github.com>
2023-06-12 12:34:46 +0300

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -28,8 +28,8 @@ void oled_Update()
     {    
     oled.ClearDisplay();
     oled.Write_Text(0,0,getSystemIPAddress());
-    oled.Write_Text(0,8,"---------");
-    oled.Write_Text(0,16,"HALIL --> QTT");
+    oled.Write_Text(0,8,"<--------->");
+    oled.Write_Text(0,32,"HALIL --> QTT");
     oled.Update();
    this_thread::sleep_for(chrono::milliseconds(100)); 
     }
