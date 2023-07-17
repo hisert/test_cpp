54c175f1defc180a330883c4bbbee1bc50dbe092
hisert <100667448+hisert@users.noreply.github.com>
2023-07-17 11:14:06 +0300

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -8,32 +8,21 @@
 #include "oled.cpp"
 #include "os.cpp"
 using namespace std;
-void oled_screen_funct();
+void WHILE_oled();
 void WHILE_serial();
+void WHILE_tcp();
 OLED oled;
 TCP tcp("192.168.1.110", 8080);
 SP sp("/dev/ttyS1");
-os_thread os_thread_oled(oled_screen_funct,100,0);
+os_thread os_thread_oled(WHILE_oled,100,0);
 os_thread os_thread_serial(WHILE_serial,10,1);
+os_thread os_thread_serial(WHILE_tcp,10,1);
 void process_tx(string data) 
 {
  if(data == "<OPI SHUTDOWN>") 
   {
      system("shutdown now");
   }
-}
-void oled_screen_funct()
-{   
-    oled.ClearDisplay();
-    oled.Rectangle(0,0,127,15,1);
-    oled.Rectangle(0,16,127,63,1);
-    oled.Write_Text((0 + 15),(0 + 4),getSystemIPAddress());
-  //  oled.Write_Text((0 + 4),(16 + 4),GetCurrentDateTime());
-    oled.Write_Text((0 + 4),(16 + 4),"TEMP->");
-    oled.Write_Text((0 + 4 +50),(16 + 4),getCPUtemperature());  
- //   oled.Write_Text((0 + 4),(48 + 4),"TIME->");
- //   oled.Write_Text((0 + 4 + 50),(48 + 4),getElapsedTimeInSeconds());       
-    oled.Update();
 }
 void INIT_oled()
 {
@@ -45,6 +34,19 @@ void INIT_oled()
 void INIT_tcp()
 {
   tcp.open();
+}
+void WHILE_oled()
+{   
+    oled.ClearDisplay();
+    oled.Rectangle(0,0,127,15,1);
+    oled.Rectangle(0,16,127,63,1);
+    oled.Write_Text((0 + 15),(0 + 4),getSystemIPAddress());
+  //  oled.Write_Text((0 + 4),(16 + 4),GetCurrentDateTime());
+    oled.Write_Text((0 + 4),(16 + 4),"TEMP->");
+    oled.Write_Text((0 + 4 +50),(16 + 4),getCPUtemperature());  
+ //   oled.Write_Text((0 + 4),(48 + 4),"TIME->");
+ //   oled.Write_Text((0 + 4 + 50),(48 + 4),getElapsedTimeInSeconds());       
+    oled.Update();
 }
 void WHILE_tcp()
 {
@@ -76,7 +78,6 @@ void WHILE_all()
   while(1)
   {
     this_thread::sleep_for(chrono::milliseconds(5));
-    WHILE_tcp();
   }
 }
 
